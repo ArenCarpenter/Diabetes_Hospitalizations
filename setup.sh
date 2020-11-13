db3fc75f6a07a7f952088c8243ed144264f74af8
Aren Carpenter <14arenc@gmail.com>
2020-11-13 12:36:41 -0500

    Add Heroku dependencies

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo “\
+[general]\n\
+email = \”14arenc@gmail.com\”\n\
+“ > ~/.streamlit/credentials.toml
+
+echo “\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+“ > ~/.streamlit/config.toml
