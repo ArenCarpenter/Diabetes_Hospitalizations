4d1200d10b36bae49e561102204c72e67d32823f
Aren Carpenter <14arenc@gmail.com>
2020-11-13 13:05:17 -0500

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit
 
-echo “[server]
+echo "[server]
 headless = true
-enableCORS=false
 port = $PORT
-“ > ~/.streamlit/config.toml
+enableCORS = false
+" > ~/.streamlit/config.toml
