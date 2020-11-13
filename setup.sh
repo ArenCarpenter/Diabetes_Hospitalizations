defcb82c5060a99e41600e231f0728a3692f5d7f
Aren Carpenter <14arenc@gmail.com>
2020-11-13 12:54:56 -0500

    Remove newline characters from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 mkdir -p ~/.streamlit/
 
-echo “\
-[general]\n\
-email = \”14arenc@gmail.com\”\n\
-“ > ~/.streamlit/credentials.toml
-
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo “[server]
+headless = true
+enableCORS=false
+port = $PORT
 “ > ~/.streamlit/config.toml
